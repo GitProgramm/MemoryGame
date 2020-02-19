97c49e94c90a37d52315750b99284cfb4062ba3e
atandersone <atandersone@DX02043385.ri.riga.lv>
2020-02-19 09:51:59 +0200

    Merge origin/master
    Conflicts:
    	css/style.css
    	index.html

--- Js/myJS.js
+++ Js/myJS.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-function tableCreate(){
+function tableCreate(table){
     var e = document.getElementById("size");
     var n = e.options[e.selectedIndex].value;
  var body = document.body,
@@ -12,8 +12,29 @@ function tableCreate(){
  tbl.style.width='100px';
  tbl.style.border='1px solid black';
 
- document.getElementById("description").innerHTML = "n";
+ document.getElementById("description").innerHTML = n;
+ let thread=table.createThread();
+for(var i =0;i<n;i++){
+    for(var k=0;k<n;k++){
+        
+    }
 }
-function test(){
-  alert("hu");  
 }
+
+
+/*select box code*/
+var colors = ['1abc9c', '2c3e50', '2980b9', '7f8c8d', 'f1c40f', 'd35400', '27ae60'];
+
+colors.each(function (color) {
+  $$('.color-picker')[0].insert(
+    '<div class="square" style="background: #' + color + '"></div>'
+  );
+});
+
+$$('.color-picker')[0].on('click', '.square', function(event, square) {
+  background = square.getStyle('background');
+  $$('.custom-dropdown select').each(function (dropdown) {
+    dropdown.setStyle({'background' : background});
+  });
+});
+
